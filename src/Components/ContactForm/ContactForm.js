67174f960a81145216de35a8eab426cf64ecb6d8
sazzadwasy <sazzad.wasy@gmail.com>
2022-06-09 21:33:39 +0600

    contact form added

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const ContactForm = () => {
     return (
-        <div class="hero min-h-screen bg-base-200">
+        <div class="hero min-h-screen py-6">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <div class="text-center lg:text-left">
-                    <h1 class="text-5xl font-bold">Login now!</h1>
-                    <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+                    <h1 class="text-5xl font-bold">Get in touch</h1>
+                    <p class="py-6">Send me an email if you think I can help you out with any of your web related projects.</p>
                 </div>
                 <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                     <div class="card-body">
--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -6,9 +6,10 @@ const Banner = () => {
             <div class="hero-content flex-col lg:flex-row">
                 <img src={Pimg} alt="" class="max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 class="text-5xl font-bold">Box Office News!</h1>
+                    <h1 class="text-5xl font-bold">Hi! I'm Sazzad</h1>
+                    <h1 class="text-3xl font-bold mt-3">Frontend Developer</h1>
                     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button class="btn btn-primary">Download Resume</button>
+                    <button class="btn btn-info">Download Resume</button>
                 </div>
             </div>
 
